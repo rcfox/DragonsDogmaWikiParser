9999c31d55f19cea12820912c08249324d2e304e
Ryan Fox <ryan.fox@upverter.com>
2017-06-03 23:55:29 -0400

    Format the tables more consistently.

--- parse_equipment.py
+++ parse_equipment.py
@@ -1,6 +1,7 @@
 import os
 import re
 import html
+import html.parser
 
 from collections import defaultdict
 
@@ -41,6 +42,92 @@ LEVEL_NAME = [
     'Gold Rarified'
 ]
 
+def icon_tag(element, image):
+    return element, '[[File:{image}|32px|link={element}|{element}]]'.format(element=element,
+                                                                            image=image)
+ELEMENT_IMAGES = {
+    'Fire': 'FIRE BASED.png',
+    'Ice': 'ICE BASED.png',
+    'Lightning': 'LIGHTNING BASED.png',
+    'Holy': 'HOLY BASED.png',
+    'Dark': 'DARK BASED.png',
+    'Darkness': 'DARK BASED.png',
+
+    'Burning': 'Fire.png',
+    'Tarring': 'Tarred.png',
+    'Tarred': 'Tarred.png',
+    'Drenched': 'DrenchedWater.png',
+    'Frozen': 'Frozen.png',
+    'Thundershock': 'LIGHTNING BASED.png',
+    'Shock': 'LIGHTNING BASED.png',
+    'Blind': 'Blindness.png',
+    'Blindness': 'Blindness.png',
+    'Curse': 'Curse.png',
+    'Cursed': 'Curse.png',
+    'Lowered Defense': 'DefenseLowered.png',
+    'Lowered Magick': 'MagickLowered.png',
+    'Lowered Magick Defense': 'MagickDefenseLowered.png',
+    'Lowered Strength': 'StrengthLowered.png',
+    'Petrification': 'Petrification.png',
+    'Poison': 'Poison.png',
+    'Possession': 'Possession.png',
+    'Silence': 'Silence.png',
+    'Skill Stifling': 'SkillStifling.png',
+    'Sleep': 'Sleep.png',
+    'Torpor': 'Torpor.png'
+}
+ELEMENT_ICONS = dict(icon_tag(element, filename) for element, filename in ELEMENT_IMAGES.items())
+
+
+def strip_html(html_data):
+    '''
+    Strip out any HTML tags, leaving only the data inside (or outside) of the tags.
+    "<b>foo</b>" -> "foo"
+    '''
+    text = []
+    class HTMLStripper(html.parser.HTMLParser):
+        ignore = False
+        def handle_starttag(self, tag, attrs):
+            # Ignore the contents of <ref> tags.
+            if tag == 'ref':
+                self.ignore = True
+
+        def handle_endtag(self, tag):
+            if tag == 'ref':
+                self.ignore = False
+
+        def handle_data(self, data):
+            if not self.ignore:
+                text.append(data.strip())
+
+    stripper = HTMLStripper()
+    stripper.feed(html_data)
+    return ' '.join(text)
+
+
+def red_text(text):
+    return '<span style="color: #f00">{text}</span>'.format(text=text)
+
+
+def parse_value(key, value):
+    text = strip_html(html.unescape(value)).replace('\n', '').replace('&nbsp;', ' ')
+    if key.startswith('element') or key.startswith('debil'):
+        element_stats = {}
+        elements = text.split('[[')[1:]
+        for element in elements:
+            key, value = re.sub(r'\|.*?\]\]', ']]', element).split(']]')
+            value = value.strip()
+            if value.startswith('-'):
+                value = red_text(value)
+            element_stats[key.strip()] = value
+        return element_stats
+    else:
+        text = text.strip()
+        if len(text) > 1 and text.startswith('-'):
+            text = red_text(text)
+
+    return text
+
 
 def parse_templates(text):
     '''
@@ -68,13 +155,13 @@ def parse_templates(text):
             bracket_count -= 1
 
         elif character == '|' or character == '}':
-        
+
             if brace_count > 1 and bracket_count == 0:
                 param = text[last_index : index].strip()
                 last_index = index + 1
                 if '=' in param:
-                    key, value = re.split('\s*=\s*', param, 1)
-                    parameters[key] = value.strip()
+                    key, value = re.split(r'\s*=\s*', param, 1)
+                    parameters[key] = parse_value(key, value)
                 else:
                     template_name = param.strip()
 
@@ -92,11 +179,11 @@ def parse_templates(text):
 def parse_page_text(filename):
     with open(filename) as f:
         text = f.read()
-        title = re.search('<title>(.*?)</title>', text).group(1)
+        title = re.search(r'<title>(.*?)</title>', text).group(1)
         # Ignore sets for now
         if ' set' in title.lower() or 'category' in title.lower():
             return None, title, {}
-        
+
         templates = parse_templates(text)
         item_type = None
         for template in templates.values():
@@ -128,8 +215,8 @@ def compile_armor(name, stats):
     keys = ['def', 'mdef', 'pierce', 'strike', 'element',
             'stagger', 'knockdown', 'debil', 'req', 'weight']
     return compile_item(name, stats, levelless_stats, keys)
-    
-    
+
+
 def compile_item(name, stats, levelless_stats, keys):
     stats_by_level = []
     for i in range(7):
@@ -193,12 +280,22 @@ def build_table(key_mapping, rows):
         row_data = []
         for key, text in key_mapping:
             comment = ' <!-- %s -->' % text.replace('<br/>', ' ').strip()
-            cell_data = html.unescape(row[key]).replace('\n', ' ').replace('&nbsp;', ' ')
+            if isinstance(row[key], dict):
+                cell_data = build_element_table(row[key])
+            else:
+                cell_data = row[key]
             row_data.append(cell_sep + cell_data + comment)
         table.append(row_sep + '\n'.join(row_data))
 
     table.append('\n|}')
     return '\n'.join(table)
+
+
+def build_element_table(element):
+    rows = []
+    for key, value in element.items():
+        rows.append('\n|-\n ! %s || %s' % (ELEMENT_ICONS[key], value))
+    return '\n{| ' + ' '.join(rows) + ' \n|}'
 
 
 def compile_all_data():
@@ -223,7 +320,7 @@ def compile_all_data():
                         category_items[category].append(compile_armor(name, stats))
     return category_items
 
-        
+
 if __name__ == '__main__':
     import sys
     category = ' '.join(sys.argv[1:]).strip()
